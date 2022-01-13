7f683526acc5e9bc7305d65ed2a532c894033248
rcmerci <rcmerci@gmail.com>
2022-01-13 17:31:04 +0800

    fix(sync): fix lint

--- src/main/frontend/components/sidebar.cljs
+++ src/main/frontend/components/sidebar.cljs
@@ -6,7 +6,6 @@
             [frontend.components.journal :as journal]
             [frontend.components.repo :as repo]
             [frontend.components.right-sidebar :as right-sidebar]
-            [frontend.components.settings :as settings]
             [frontend.components.theme :as theme]
             [frontend.components.widgets :as widgets]
             [frontend.components.plugins :as plugins]
@@ -453,7 +452,6 @@
                 state)}
   [state route-match main-content]
   (let [{:keys [open-fn]} state
-        me (state/sub :me)
         current-repo (state/sub :git/current-repo)
         granted? (state/sub [:nfs/user-granted? (state/get-current-repo)])
         theme (state/sub :ui/theme)
